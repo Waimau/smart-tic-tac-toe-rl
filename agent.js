eed1c7315b33281e8870826d78298add73021b1d
Waimau <144480066+Waimau@users.noreply.github.com>
2021-04-02 17:42:17 +0000

    refact.

--- agent.js
+++ agent.js
@@ -2,4 +2,5 @@ class Agent{
 
   constructor(name, eps=0.05, alpha=0.5) {
     this.eps = eps; // probability of choosing random action instead of greedy
-    this.alpha = alpha; // 
+    this.alpha = alpha; // learning rate
+    th
