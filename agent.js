f84454292901b898d8bf2020e81988ad91bb5dd6
Waimau <144480066+Waimau@users.noreply.github.com>
2021-04-02 21:10:12 +0000

    Run scan-local.sh on gazebo:...

--- agent.js
+++ agent.js
@@ -3,4 +3,4 @@ class Agent{
   constructor(name, eps=0.05, alpha=0.5) {
     this.eps = eps; // probability of choosing random action instead of greedy
     this.alpha = alpha; // learning rate
-    th
+    this.state_his
