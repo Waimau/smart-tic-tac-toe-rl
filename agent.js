8339d6da0c7f3e00c1044a08417c8eafefdff005
Waimau <144480066+Waimau@users.noreply.github.com>
2021-04-03 13:20:40 +0000

    Clang-format

--- agent.js
+++ agent.js
@@ -3,4 +3,5 @@ class Agent{
   constructor(name, eps=0.05, alpha=0.5) {
     this.eps = eps; // probability of choosing random action instead of greedy
     this.alpha = alpha; // learning rate
-    this.state_his
+    this.state_history = [];
+    th
