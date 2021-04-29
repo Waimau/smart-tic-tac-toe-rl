b95963e226aba8da68bc4ea7e5f6a48397104cb4
Waimau <144480066+Waimau@users.noreply.github.com>
2021-04-29 07:49:29 +0000

    More description

--- agent.js
+++ agent.js
@@ -33,4 +33,6 @@ class Agent{
       let possible_moves = [];
       for(let i=0;i<env.board.length;i++){
         if(env.board[i]==0){
-          possible_moves.push(
+          possible_moves.push(i);
+        }
+      }
