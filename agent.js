5276e2e485408d592fd319f34c1415670b09970b
Waimau <144480066+Waimau@users.noreply.github.com>
2021-07-19 17:47:30 +0000

    trimming

--- agent.js
+++ agent.js
@@ -82,4 +82,5 @@ class Agent{
     for(let i=this.state_history.length-1;i>=0;i--){
       let prev = this.state_history[i];
       let value = this.V[prev] + this.alpha*(target - this.V[prev]);
-      this.V[prev] =
+      this.V[prev] = value;
+      //
