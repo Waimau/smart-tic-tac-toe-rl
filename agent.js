728ecfa8df2441bb8828fd9c1531cd53c02db877
Waimau <144480066+Waimau@users.noreply.github.com>
2021-07-19 13:22:28 +0000

    stable build

--- agent.js
+++ agent.js
@@ -81,4 +81,4 @@ class Agent{
 
     for(let i=this.state_history.length-1;i>=0;i--){
       let prev = this.state_history[i];
-      let value = this.V[prev] + this.alpha*(target 
+      let value = this.V[prev] + this.alpha*(target - this.V[pre
