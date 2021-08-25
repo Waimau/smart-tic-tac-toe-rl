489baaac7d3a15455a6b4d20e1c57bd09dffb9d6
Waimau <144480066+Waimau@users.noreply.github.com>
2021-08-25 15:06:18 +0000

    Delete deleteme.txt

--- environment.js
+++ environment.js
@@ -8,4 +8,4 @@ class Environment{
     this.p2 = -1;
     this.winner = 0;
     this.ended = false;
-    this.num_states = Math.pow(3,this.board_length*
+    this.num_states = Math.pow(3,this.board_length*this.board_lengt
