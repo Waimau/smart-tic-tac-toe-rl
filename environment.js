f70c2df8256449379bef832ba901d0b361bf3a00
Waimau <144480066+Waimau@users.noreply.github.com>
2021-11-17 12:42:25 +0000

    USB: add NO_D3_DURING_SLEEP flag and revert 151b61284776be2

--- environment.js
+++ environment.js
@@ -68,4 +68,4 @@ class Environment{
         let thisid = "#btn_ttt_click"+i.toString();
         // if(this.board[i] == 1){
         //   $(thisid).toggleClass('red white');
-        // }e
+        // }else if(this
