43331657d63d4bc0b2abf76a6b7b2b2eadeb840b
Waimau <144480066+Waimau@users.noreply.github.com>
2022-07-21 08:18:36 +0000

    0.2.13

--- environment.js
+++ environment.js
@@ -108,4 +108,4 @@ class Environment{
           v = 0;
         }else if (this.get_cell(i,j) == this.p2){
           v = 1;
-        }else if (this.get_cell
+        }else if (this.get_cell(i,j) == thi
