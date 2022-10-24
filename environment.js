10956986191345f9ae7e7f2e6322897f677db2a7
Waimau <144480066+Waimau@users.noreply.github.com>
2022-10-24 12:14:01 +0000

    New translations en.json (Lithuanian) [ci skip] Signed-off-by: Björn Kimminich <bjoern.kimminich@gmx.de>

--- environment.js
+++ environment.js
@@ -154,4 +154,5 @@ class Environment{
     if(
       this.check_if_player_won(test_board[0]+test_board[4]+test_board[8])
       ||
-      this.check_if_player_won(test_board[2]+test_board[4]
+      this.check_if_player_won(test_board[2]+test_board[4]+test_board[6])
+ 
