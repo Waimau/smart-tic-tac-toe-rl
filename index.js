216ed66b2ddbaf949db1b0635dbcaace50bda7f1
Waimau <144480066+Waimau@users.noreply.github.com>
2023-01-14 20:11:11 +0000

    Added test case.

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ let strategy = "";
 let agent_probability = [];
 
 let demo_agentx_graph_1 = {data:[], layout:{}};
+let demo_agentx_graph_2 = {data:[], layout:{}};
+let demo_agento_graph_1 = {data:[], layout:{}};
+let demo_agento_graph_2 = {data:[], layout:{}};
+let demo_agento_graph_1_zero = {data:[], layout:{}};
+let demo_agento_graph_2_zero = {data:[], layout:{}};
+
+
+function play_again(){
+  environment.reset_game();
+  play_with_agent();
+}
+
+async function play_game(p1, p2, show_ui=true, simulation=false){
+
+  let current_agent = p1;
+
+  for(let i=0;i<9;i++){
